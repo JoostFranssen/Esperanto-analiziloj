5875f546af9338bec6c7d0ce1714c4ccb92e581c
Joost Franssen <jfranssen@sogyo.nl>
2019-11-05 11:40:52 +0100

    Switch from SAX to DOM; add determining whether a ReVo entry is a root or not

--- src/main/java/nl/sogyo/esperanto/persistence/ReVoLegilo.java
+++ src/main/java/nl/sogyo/esperanto/persistence/ReVoLegilo.java
@@ -1,16 +1,18 @@
 package nl.sogyo.esperanto.persistence;
 
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.parsers.SAXParserFactory;
 
-import org.xml.sax.Attributes;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
-import org.xml.sax.XMLReader;
 import org.xml.sax.helpers.DefaultHandler;
 
 import nl.sogyo.esperanto.API.VorterSpeco;
@@ -22,63 +24,58 @@ public class ReVoLegilo extends DefaultHandler {
 	private String dosierPado;
 	private ReVoEnigo enigo;
 	
-	private boolean vorteroTrovita, vorterSpecoTrovita, transitivecoTrovita;
-	private String legendaElemento = "";
-	private StringBuilder legatajSignoj;
-	
 	public ReVoLegilo(String dosierPado) {
 		this.dosierPado = dosierPado;
 		enigo = new ReVoEnigo();
-		legatajSignoj = new StringBuilder();
 	}
 	
 	public ReVoEnigo legiDosieron() {
+		DocumentBuilderFactory fabriko = DocumentBuilderFactory.newInstance();
+		
 		try {
-			XMLReader legilo = SAXParserFactory.newInstance().newSAXParser().getXMLReader();
-			legilo.setContentHandler(this);
-			legilo.parse(dosierPado);
+			DocumentBuilder konstruilo = fabriko.newDocumentBuilder();
+			Document dokumento = konstruilo.parse(dosierPado);
+			Element ĉefElemento = dokumento.getDocumentElement();
+			ĉefElemento.normalize();
+			
+			enigo.setVortero(ekstraktiVorteron(ĉefElemento));
+			
+			NodeList kapvortNodoj = ĉefElemento.getElementsByTagName(ETIKEDO_KAPVORTO);
+			
+			if(kapvortNodoj.getLength() > 0) {
+				String finaĵo = ekstraktiFinaĵonDeKapvorto(kapvortNodoj.item(0));
+				if(!finaĵo.isEmpty()) {
+					enigo.setVorterSpeco(VorterSpeco.RADIKO);
+				}
+			}
+			
+			
 		} catch(ParserConfigurationException | SAXException | IOException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
 		
 		return enigo;
 	}
 	
-	@Override
-	public void startElement(String uri, String localName, String qName, Attributes attributes) {
-		if(!vorteroTrovita) {
-			if(qName.equalsIgnoreCase(ETIKEDO_RADIKO)) {
-				legendaElemento = ETIKEDO_RADIKO;
-			}
+	private String ekstraktiVorteron(Element ĉefElemento) {
+		NodeList nodoj = ĉefElemento.getElementsByTagName(ETIKEDO_RADIKO);
+		if(nodoj.getLength() > 0) {
+			return nodoj.item(0).getTextContent();
 		}
+		return "";
 	}
 	
-	@Override
-	public void endElement(String uri, String localName, String qName) {
-		if(!qName.equalsIgnoreCase(legendaElemento)) {
-			return;
-		}
+	private String ekstraktiFinaĵonDeKapvorto(Node kapvortNodo) {
+		String enhavo = kapvortNodo.getTextContent();
 		
-		legendaElemento = "";
-		String teksto = legatajSignoj.toString();
-		legatajSignoj = new StringBuilder();
-		
-		if(qName.equalsIgnoreCase(ETIKEDO_RADIKO)) {
-			setVortero(teksto);
+		Pattern ŝablono = Pattern.compile(String.format("(%s)/(\\S*)", enigo.getVortero()));
+		Matcher kongruilo = ŝablono.matcher(enhavo);
+		if(kongruilo.find()) {
+			return kongruilo.group(2);
+		} else {
+			return "";
 		}
-	}
-	
-	@Override
-	public void characters(char[] ch, int start, int length) {
-		if(!legendaElemento.isEmpty()) {
-			for(int i = start; i < start + length; i++) {
-				legatajSignoj.append(ch[i]);
-			}
-		}
-	}
-	
-	private void setVortero(String vortero) {
-		enigo.setVortero(vortero);
-		vorteroTrovita = true;
 	}
 }
