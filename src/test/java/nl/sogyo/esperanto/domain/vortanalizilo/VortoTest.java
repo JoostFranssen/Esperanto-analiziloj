c50be56331accb5663eb5431026df2fe352ac612
Joost Franssen <jfranssen@sogyo.nl>
2019-11-07 15:42:22 +0100

    Remove unnecessary import

--- src/test/java/nl/sogyo/esperanto/domain/vortanalizilo/VortoTest.java
+++ src/test/java/nl/sogyo/esperanto/domain/vortanalizilo/VortoTest.java
@@ -3,9 +3,6 @@ package nl.sogyo.esperanto.domain.vortanalizilo;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
-
-import nl.sogyo.esperanto.API.Trajto;
-
 class VortoTest {
 
 	@Test
