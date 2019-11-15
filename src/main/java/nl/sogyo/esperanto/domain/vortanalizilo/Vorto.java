a45fdf5017e3f3918c4725613332423a4360574a
Joost Franssen <jfranssen@sogyo.nl>
2019-11-15 16:12:02 +0100

    Add a filter to never read the suffixes j, ĉj and nj, as they are not supported anyway because of their abnormal application

--- src/main/java/nl/sogyo/esperanto/domain/vortanalizilo/Vorto.java
+++ src/main/java/nl/sogyo/esperanto/domain/vortanalizilo/Vorto.java
@@ -5,35 +5,46 @@ import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.function.Predicate;
 
+import nl.sogyo.esperanto.API.IVortero;
 import nl.sogyo.esperanto.API.VorterSpeco;
 
 /**
  * Analizita vorto.
+ * 
  * @author jfranssen
  *
  */
 public class Vorto {
 	private String vorto;
 	private Set<Analizaĵo> possibleAnalizaĵoj;
+	private List<Analizaĵo> sortedAnalizaĵoj;
 	
+	public static Predicate<IVortero> defaultFilterForDatabase = v -> {
+		return v.getVorterSpeco() != VorterSpeco.LITERO && !v.equals(Vortero.J_SUFIKSO) && !v.equals(Vortero.ĈJ_SUFIKSO) && !v.equals(Vortero.NJ_SUFIKSO);
+	};
+
 	/**
-	 * Iniciatas novan vorton, analizas ĝin por trovi {@code Analizaĵo}jn, kiuj poste estas filtritaj laŭ ĝia valideco.
+	 * Iniciatas novan vorton, analizas ĝin por trovi {@code Analizaĵo}jn, kiuj
+	 * poste estas filtritaj laŭ ĝia valideco.
+	 * 
 	 * @param vorto
 	 */
 	public Vorto(String vorto) {
 		this(vorto, true);
 	}
+
 	public Vorto(String vorto, boolean filterInvalidAnalizaĵoj) {
 		this.vorto = vorto;
 		possibleAnalizaĵoj = new HashSet<>();
-		
+
 		analyze();
 		if(filterInvalidAnalizaĵoj) {
 			filterInvalidAnalizaĵoj();
 		}
 	}
-	
+
 	/**
 	 * Iniciatas la aron da {@code Analizaĵo}j.
 	 */
@@ -41,10 +52,10 @@ public class Vorto {
 		if(vorto.isEmpty()) {
 			possibleAnalizaĵoj.add(new Analizaĵo());
 		}
-		
+
 		for(int i = vorto.length() - 1; i >= 0; i--) {
 			String takenPart = vorto.substring(i);
-			List<Vortero> result = DatabaseCommunicator.getFromDatabase(takenPart, v -> v.getVorterSpeco() != VorterSpeco.LITERO);
+			List<Vortero> result = DatabaseCommunicator.getFromDatabase(takenPart, defaultFilterForDatabase);
 			if(!result.isEmpty()) {
 				Vorto subVorto = new Vorto(vorto.substring(0, i), false);
 				subVorto.analyze();
@@ -58,14 +69,14 @@ public class Vorto {
 			}
 		}
 	}
-	
+
 	/**
 	 * Filtras la {@code Analizaĵo}jn laŭ valideco.
 	 */
 	private void filterInvalidAnalizaĵoj() {
 		possibleAnalizaĵoj.removeIf(a -> !a.isValid());
 	}
-	
+
 	public String getVorto() {
 		return vorto;
 	}
@@ -73,17 +84,28 @@ public class Vorto {
 	public Set<Analizaĵo> getPossibleAnalizaĵoj() {
 		return new HashSet<>(possibleAnalizaĵoj);
 	}
-	
+
 	public List<Analizaĵo> getPossibleAnalizaĵojSorted() {
-		ArrayList<Analizaĵo> possibleAnalizaĵojList = new ArrayList<>(possibleAnalizaĵoj);
-		Collections.sort(possibleAnalizaĵojList, Analizaĵo.getComparator());
-		return possibleAnalizaĵojList;
+		if(sortedAnalizaĵoj == null) {
+			sortedAnalizaĵoj = new ArrayList<>(possibleAnalizaĵoj);
+			Collections.sort(sortedAnalizaĵoj, Analizaĵo.getComparator());
+		}
+		return sortedAnalizaĵoj;
 	}
 	
+	public Analizaĵo getFirstAnalizaĵo() {
+		return getPossibleAnalizaĵojSorted().get(0);
+	}
+
 	/**
-	 * Prenas unu el la eblaj {@code Analizaĵo}j laŭ la donita ŝablono. Redonas {@code null}, se neniu estas trovata.
-	 * @param string {@code String}-objekto, kiu reprezentas la vort-dividon de la dezirata {@code Analizaĵo}. Vertikala streko funkciu por la divido; ekzemple: "esper|ant|o"
-	 * @return unu el la eblaj {@code Analizaĵo}j, kiu kongruas kun {@code dividaĈeno}
+	 * Prenas unu el la eblaj {@code Analizaĵo}j laŭ la donita ŝablono. Redonas
+	 * {@code null}, se neniu estas trovata.
+	 * 
+	 * @param string {@code String}-objekto, kiu reprezentas la vort-dividon de la
+	 *			   dezirata {@code Analizaĵo}. Vertikala streko funkciu por la
+	 *			   divido; ekzemple: "esper|ant|o"
+	 * @return unu el la eblaj {@code Analizaĵo}j, kiu kongruas kun
+	 *		 {@code dividaĈeno}
 	 */
 	public Analizaĵo getAnalizaĵoByString(String string) {
 		for(Analizaĵo analizaĵo : possibleAnalizaĵoj) {
@@ -93,7 +115,7 @@ public class Vorto {
 		}
 		return null;
 	}
-	
+
 	@Override
 	public String toString() {
 		if(possibleAnalizaĵoj.size() > 0) {
